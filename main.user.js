31cbb86ae23bb9f2cc45b39de7a0a2283d86f9c8
TaroXin <15029352778@163.com>
2022-12-30 17:24:45 +0800

    feat: version 1.1.0

--- main.user.js
+++ main.user.js
@@ -2,15 +2,15 @@
 // ==UserScript==
 // @name           center-google-search
 // @namespace      taroxin/center-google-search
-// @version        1.0.0
+// @version        1.1.0
 // @author         taroxin
-// @include        *
+// @description    Centering Google's search page
+// @include        https://www.google.com/search*
 // @homepage       https://github.com/TaroXin/center-google-search#readme
 // @supportURL     https://github.com/TaroXin/center-google-search/issue
 // @updateURL      https://raw.githubusercontent.com/TaroXin/center-google-search/main/main.user.js
 // @downloadURL    https://raw.githubusercontent.com/TaroXin/center-google-search/main/main.user.js
-// @run-at         document-end
-// @match          https://www.google.com/search*
+// @run-at         document-start
 // ==/UserScript==
 (function () {
 	'use strict';
